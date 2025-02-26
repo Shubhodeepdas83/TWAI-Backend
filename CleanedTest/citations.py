c48212f02bac5c520ab74888eab834b29366319f
harshj3915 <hjain3915@gmail.com>
2025-02-26 21:23:48 +0530

    Trial and error

--- CleanedTest/citations.py
+++ CleanedTest/citations.py
@@ -7,11 +7,10 @@ import re
 import uuid
 import hashlib
 import base64
-import en_core_web_sm
 load_dotenv()
 
 # Load spaCy model for natural language processing
-nlp = en_core_web_sm.load()
+nlp = spacy.load("en_core_web_sm")
 
 
 
